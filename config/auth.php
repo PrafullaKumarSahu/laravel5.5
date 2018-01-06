efd82d0d12e9a945eae31672c0f94dc8afc0f19c
prafullakumarsahu <prafullakumarsahu001@gmail.com>
2018-01-06 23:43:53 +0530

    Implementing Entrust - Creating models and controllers using artisan

--- config/auth.php
+++ config/auth.php
@@ -77,7 +77,7 @@ return [
 
         'users' => [
             'driver' => 'eloquent',
-            'model' => Namespace\Of\Your\User\Model\User::class,
+            'model' => App\User::class,
             'table' => 'users',
         ],
     ],
